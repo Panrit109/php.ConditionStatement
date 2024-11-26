d0a693f6f7062ee9a40b129b9d75c7e5f1d8b35c
Panrit109 <wolwolwol11@gmail.com>
2024-11-26 12:06:36 +0700

    love

---
+++ demo.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>ทดสอบส่งค่าโดยใช้ Method GET</title>
+</head>
+<body>
+    <h2>ทดสอบรับค่าจาก method GET (ผ่าน URL)</h2>
+    <a href="test_get.php?subject=PHP&web=ww.google.com">Click</a>
+</body>
+</html>
---
+++ test_get.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>ทดสอบส่งค่าโดยใช้ Method GET</title>
+</head>
+<body>
+    <h2>ทดสอบรับค่าจาก method GET (ผ่าน URL)</h2>
+    <?php
+    echo"ศึกษาเรื่อง : ".$_GET['subject']." ได้ที่ ".$_GET['web'];
+    
+    ?>
+</body>
+</html>
